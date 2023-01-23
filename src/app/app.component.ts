51ce469b3955f22b61514bddd6c378fac7403a9b
Sofia Calderon <scalderonj204@gmail.com>
2023-01-23 16:56:17 -0600

    Primer Laboratorio
    Laboratorio creado en grupo el primer dia de clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  titulo='Adopta Una Mascota';
+  descripcion: string= 'A través de la historia, las mascotas han sido grandes compañeras de la vida de las personas, compartiendo alegrías, pero también ayudando a superar momentos difíciles.'
+  img='https://thecatapi.com/api/images/get?format=src&type=gif'
 }
